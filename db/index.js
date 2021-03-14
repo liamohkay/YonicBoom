64cfa9204f268e4e6a40040b6da8d796925e9553
liamohkay <liam.olsonkenny@gmail.com>
2021-03-14 16:40:03 -0700

    Compelted seeding for artists w/ aliases

--- db/index.js
+++ db/index.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql');
 const config = require('./config.js');
+const runSeed = require('./seed.js');
 
 const connection = mysql.createConnection(config);
 connection.connect(() => console.log(`Connected to ${config.database}`));
+
+runSeed(connection);
--- db/seed.js
+++ db/seed.js
@@ -2,23 +2,35 @@ const path = require('path');
 const mysql = require('mysql');
 const xlsx = require('node-xlsx');
 
-const seedDB = () =>  {
+const runSeed = (db) =>  {
   let seedData = xlsx.parse(path.join(__dirname, 'seed.xlsx'));
   seedData = seedData[0].data.slice(1, seedData[0].data.length);
   // structure: arist, genre, pronouns, scbc, link
   seedData.map(row => {
-    // Get artist aliases + insert into alias table
     if (row[0].includes(',')) {
       let artistNames = row[0].split(', ');
+      let artist = artistNames[0];
       let aliases = artistNames.slice(1, artistNames.length);
+
+      let artistQuery = `
+        INSERT INTO artist (artist, genre, pronouns, scbc, link)
+        VALUES ('${artist}', '${row[1]}', '${row[2]}', '${row[3]}', '${row[4]}');`
+      db.query(artistQuery, () => {});
+
+
       aliases.map(alias => {
-        mysql.query(`INSERT INTO alias VALUES ('${artistNames[0]}', '${alias}')`)
-      })
+        let aliasQuery = `
+          INSERT INTO alias (artist, artist_alias)
+          VALUES ('${artist}', '${alias}');`
+        db.query(aliasQuery, () => {});
+      });
     }
   });
 };
 
-seedDB();
+// runSeed
+
+module.exports = runSeed;
 
 // first row is colnames
 // then map => INSERT TO
