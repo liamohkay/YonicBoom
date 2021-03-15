85f945e45dd1fbf089b4a6cb0e8138590f0a82f6
liamohkay <liam.olsonkenny@gmail.com>
2021-03-14 18:21:44 -0700

    Added more todo items, created front end search rough draft

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,25 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export const App = () => {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSearch = () => {
+    axios.get('/api/artist')
+      .then(resp => setResults(resp.data))
+      .catch(err => console.log(err))
+  }
+
   return (
-    <div id="app">
-      Hello
+    <div id="app-container">
+      <div id="app-header">
+        <h1>YonicBoom</h1>
+        <div>women, nonbinary, and trans artists in dance</div>
+      </div>
+      <div className="search-container">
+        <input type="text" onChange={(e) => setSearch(e.target.value)}></input>
+        <button onClick={handleSearch}>Search</button>
+      </div>
     </div>
   );
 }
