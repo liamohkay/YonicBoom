bf3eb79d8508da14c1a8d9e8645dffd628a304a9
Liam OK <liamohkay@gmail.com>
2023-05-20 13:40:33 -0400

    Set up API route for all artists

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { SignIn } from "@clerk/nextjs";
-import { useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
+  const allArtists = api.getAll.useQuery()
+
   
   return (
     <>
@@ -50,29 +50,10 @@ const Home: NextPage = () => {
             </Link>
           </div>
         </div>
-        <AuthShowcase />
+        { JSON.stringify(allArtists) ?? null}
       </main>
     </>
   );
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <SignIn />
-    </div>
-  );
-};
