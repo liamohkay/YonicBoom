0b7e7836f35a505805bf6a47eb20d672e37b3f73
Liam OK <liamohkay@gmail.com>
2023-05-21 17:54:06 -0400

    Added api procedure for blogs

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { api } from '~/utils/api';
 
 const Blog: React.FC = (): JSX.Element => {
+  const blogs = api.getBlogs.useQuery().data;
   return (
     <>
-      
+      {JSON.stringify(blogs)}
     </>
   )
 }
+
+export default Blog;
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,7 +4,11 @@ export const appRouter = createTRPCRouter({
   getArtists: publicProcedure.query(async ({ ctx }) => {
     const artists = await ctx.prisma.artist.findMany();
     return artists;
-  }) 
+  }),
+  getBlogs: publicProcedure.query(async ({ ctx } ) => {
+    const blogs = await ctx.prisma.blog.findMany();
+    return blogs;
+  })
 });
 
 export type AppRouter = typeof appRouter;
