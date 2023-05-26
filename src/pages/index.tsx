4f77d5e049dc8779b8f7aa60f773fbc28845c4d4
Liam OK <liamohkay@gmail.com>
2023-05-26 01:22:08 -0400

    Added tailwind template for navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import Search from '~/components/Search';
+import Navbar from '~/components/Navbar';
 
 const Home: NextPage = () => {
 
@@ -11,6 +11,7 @@ const Home: NextPage = () => {
         <title>YonicBoom</title>
         <meta name="description" content="women, nonbinary, and trans artists in dance" />
         <link rel="icon" href="/favicon.ico" />
+        <Navbar />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-[#FFEFE7]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
