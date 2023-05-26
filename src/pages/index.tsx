6583d0746ad3a3e6f0a745fdff3365104b7ff0cd
Liam OK <liamohkay@gmail.com>
2023-05-26 18:52:15 -0400

    Retiring work on navbar for now until data table is built out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from "next/head";
 import { type NextPage } from "next";
-import { useUser, SignIn } from '@clerk/clerk-react';
-import Search from '~/components/Search';
+import Discover from '~/components/Discover';
 import Navbar from '~/components/Navbar';
 
 const Home: NextPage = () => {
-
   return ( 
     <>
       <Head>
@@ -13,10 +11,9 @@ const Home: NextPage = () => {
         <meta name="description" content="women, nonbinary, and trans artists in dance" /> 
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-center bg-[#FFEFE7]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <Search />
+          <Discover />
         </div>
       </main>
     </>
