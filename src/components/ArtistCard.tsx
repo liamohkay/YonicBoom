f6fb4faeed6b976c80cb9250fa50d8b1911b2d8f
Liam OK <liamohkay@gmail.com>
2023-05-30 22:12:55 -0400

    Tailwind is frustrating

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -12,20 +12,21 @@ interface IDictionary<TValue> {
   [id: string]: TValue;
 }
 const ArtistCard: React.FC<ArtistCardProps> = ({ artist, color }): JSX.Element => {
-  console.log(color)
   return (
     <div className="w-1/5 min-w-[25rem] rounded shadow-lg bg-white">
       <div className="flex px-6 py-4">
         <Image src='/no-pic.png' alt="noPicture" width="75" height="75" />
         <div className="flex flex-col px-5">
           <div>
-            <span className="font-bold text-l">{artist.artist}</span><span className="text-slate-500">{` (${artist.pronoun})`}</span>
+            <span className="font-bold text-l">{artist.artist}</span><span className="text-slate-400">{` (${artist.pronoun})`}</span>
           </div>
           <span className={color}>{artist.genre}</span>
         </div>
-        <a href={artist.artistUrl}>
-          <Image src={artist.artistUrl.includes('bandcamp') ? "/bc.png" : "/sc.png" } alt="artistProfile" width="25" height="25" />
-        </a>
+        <div className="flex-row"></div>
+          <a href={artist.artistUrl}>
+            <Image src={artist.artistUrl.includes('bandcamp') ? "/bc.png" : "/sc.png" } alt="artistProfile" width="25" height="25" />
+          </a>
+          {/* <Image src='/headphone-off.png' alt="headoff" width="50" height="25" /> */}
       </div>
     </div>
   )
