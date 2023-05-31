40f632eb32498120b8e7fce663fa6f1ad872fb8d
Liam OK <liamohkay@gmail.com>
2023-05-31 16:20:06 -0400

    Fixed about type errors

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,10 +7,10 @@ const About: NextPage = () => {
         <span className="text-xl font-bold mt-4">YonicBoom!</span> 
         <br />
         <p className="text-m md:w-8/12">
-        YonicBoom! is a radio show based out of Hollow Earth Radio, in Seattle's Capitol Hill area. YB has taken many forms over the years, and currently is run by Andrea Nela. It began as a skill share show in 2014 and has been on air ever since.
+        {`YonicBoom! is a radio show based out of Hollow Earth Radio, in Seattle's Capitol Hill area. YB has taken many forms over the years, and currently is run by Andrea Nela. It began as a skill share show in 2014 and has been on air ever since.`}
         <br />
         <br />
-        This database will serve to highlight women and non-binary artists across all sub genres of dance music. If you're interested in adding a name, drop a line! 
+        {`This database will serve to highlight women and non-binary artists across all sub genres of dance music. If you're interested in adding a name, drop a line!`}
         <br />
         <br />
         This space will be used in future to highlight artists and releases. 
